8d50e72ba70f96b6a645f22c56e9a7c42a5934fc
ShibCode <100206484+ShibCode@users.noreply.github.com>
2024-07-16 10:44:09 +0500

    deploy

--- src/app/leaderboards/page.jsx
+++ src/app/leaderboards/page.jsx
@@ -48,94 +48,97 @@ const Leaderboards = () => {
   }, [activeMode]);
 
   return (
-    <div className="wrapper my-12">
-      <div className="contain flex-col gap-8">
-        <h1 className="text-tertiary text-5xl mb-2">Leaderboards</h1>
+    leaderboards && (
+      <div className="wrapper my-12">
+        <div className="contain flex-col gap-8">
+          <h1 className="text-tertiary text-5xl mb-2">Leaderboards</h1>
 
-        <div className="flex gap-2">
-          {MODES.map((mode, index) => (
-            <button
-              key={index}
-              onClick={() => changeActiveMode(mode)}
-              className={`px-4 py-2 rounded-lg ${
-                mode === activeMode
-                  ? "bg-tertiary text-bgSecondary"
-                  : "bg-bgSecondary text-tertiary"
-              }`}
-            >
-              {mode}
-            </button>
-          ))}
-        </div>
-        {!isFetching && leaderboards ? (
-          <table className="relative">
-            <thead>
-              <tr className="text-primary text-xs">
-                {tableHeadings.map((heading, index) => (
-                  <td
-                    key={index}
-                    tooltip={
-                      heading.onHoverTooltip ? heading.onHoverTooltip : ""
-                    }
-                    className={`p-2 select-none ${index === 0 && "pl-4"} ${
-                      heading.onHoverTooltip && "hover-tooltip"
-                    }
-      `}
-                  >
-                    <span>{heading.name}</span>
-                  </td>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {leaderboards.length > 0 ? (
-                leaderboards.map((test, index) => (
-                  <tr
-                    key={index}
-                    className={`${
-                      index % 2 === 0 ? "bg-bgSecondary" : "bg-transparent"
-                    } ${
-                      test.username === user.username
-                        ? "text-secondary"
-                        : "text-tertiary"
-                    }`}
-                  >
-                    <td className="p-2 pl-4">
-                      <div className="w-[18px] text-center">
-                        {index === 0 ? (
-                          <FontAwesomeIcon icon={faCrown} />
-                        ) : (
-                          index + 1
-                        )}
-                      </div>
-                    </td>
-                    <td className="p-2">{test.username}</td>
-                    <td className="p-2">{test.wpm}</td>
-                    <td className="p-2">{test.raw}</td>
-                    <td className="p-2">{test.accuracy}%</td>
-                    <td className="p-2">
-                      {test.correct}/{test.incorrect}/{test.extra}/{test.missed}
-                    </td>
-                    <td className="p-2">
-                      <div>{test.date}</div>
-                      <div>{test.time}</div>
-                    </td>
-                  </tr>
-                ))
-              ) : (
-                <div className="absolute w-full text-center text-tertiary my-10">
-                  No data found!
-                </div>
-              )}
-            </tbody>
-          </table>
-        ) : (
-          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Spinner />
+          <div className="flex gap-2">
+            {MODES.map((mode, index) => (
+              <button
+                key={index}
+                onClick={() => changeActiveMode(mode)}
+                className={`px-4 py-2 rounded-lg ${
+                  mode === activeMode
+                    ? "bg-tertiary text-bgSecondary"
+                    : "bg-bgSecondary text-tertiary"
+                }`}
+              >
+                {mode}
+              </button>
+            ))}
           </div>
-        )}
+          {!isFetching && leaderboards ? (
+            <table className="relative">
+              <thead>
+                <tr className="text-primary text-xs">
+                  {tableHeadings.map((heading, index) => (
+                    <td
+                      key={index}
+                      tooltip={
+                        heading.onHoverTooltip ? heading.onHoverTooltip : ""
+                      }
+                      className={`p-2 select-none ${index === 0 && "pl-4"} ${
+                        heading.onHoverTooltip && "hover-tooltip"
+                      }
+      `}
+                    >
+                      <span>{heading.name}</span>
+                    </td>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {leaderboards.length > 0 ? (
+                  leaderboards.map((test, index) => (
+                    <tr
+                      key={index}
+                      className={`${
+                        index % 2 === 0 ? "bg-bgSecondary" : "bg-transparent"
+                      } ${
+                        test.username === user.username
+                          ? "text-secondary"
+                          : "text-tertiary"
+                      }`}
+                    >
+                      <td className="p-2 pl-4">
+                        <div className="w-[18px] text-center">
+                          {index === 0 ? (
+                            <FontAwesomeIcon icon={faCrown} />
+                          ) : (
+                            index + 1
+                          )}
+                        </div>
+                      </td>
+                      <td className="p-2">{test.username}</td>
+                      <td className="p-2">{test.wpm}</td>
+                      <td className="p-2">{test.raw}</td>
+                      <td className="p-2">{test.accuracy}%</td>
+                      <td className="p-2">
+                        {test.correct}/{test.incorrect}/{test.extra}/
+                        {test.missed}
+                      </td>
+                      <td className="p-2">
+                        <div>{test.date}</div>
+                        <div>{test.time}</div>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <div className="absolute w-full text-center text-tertiary my-10">
+                    No data found!
+                  </div>
+                )}
+              </tbody>
+            </table>
+          ) : (
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+              <Spinner />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
