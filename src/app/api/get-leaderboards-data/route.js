d398df09c4cc55447ac1541ece31ff06aa34cc1e
ShibCode <100206484+ShibCode@users.noreply.github.com>
2025-04-01 15:03:50 +0500

    apply cronjob for leaderboards

--- src/app/api/get-leaderboards-data/route.js
+++ src/app/api/get-leaderboards-data/route.js
@@ -1,70 +1,17 @@
-import { Test } from "@/model/test";
 import dbConnect from "@/utils/dbConn";
 import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
   try {
-    await dbConnect();
-
-    const modes = [
-      { text: "Words 10", mode: { name: "words", category: 10 } },
-      { text: "Words 25", mode: { name: "words", category: 25 } },
-      { text: "Words 50", mode: { name: "words", category: 50 } },
-      { text: "Words 100", mode: { name: "words", category: 100 } },
-      { text: "Time 15", mode: { name: "time", category: 15 } },
-      { text: "Time 30", mode: { name: "time", category: 30 } },
-      { text: "Time 60", mode: { name: "time", category: 60 } },
-      { text: "Time 120", mode: { name: "time", category: 120 } },
-    ];
+    const { connection: db } = await dbConnect();
 
     const { mode } = await req.json();
 
-    const activeMode = modes.find((c) => c.text === mode)?.mode;
+    const collection = db.collection(mode);
 
-    if (!activeMode) {
-      return NextResponse.json({
-        success: false,
-        message: "Invalid category",
-      });
-    }
+    const data = await collection.find({}).toArray();
 
-    const top100 = await Test.aggregate([
-      {
-        $match: {
-          "mode.name": activeMode.name,
-          "mode.category": activeMode.category,
-        },
-      },
-      {
-        $project: {
-          timeTaken: 0,
-          isPersonalBest: 0,
-          mode: 0,
-          language: 0,
-          __v: 0,
-          _id: 0,
-        },
-      },
-      { $sort: { wpm: -1 } },
-      { $group: { _id: "$userId", fastestTest: { $first: "$$ROOT" } } },
-      { $replaceRoot: { newRoot: "$fastestTest" } },
-      { $sort: { wpm: -1 } },
-      { $limit: 100 },
-      {
-        $lookup: {
-          from: "users",
-          localField: "userId",
-          foreignField: "_id",
-          as: "user",
-          pipeline: [{ $project: { _id: 0, username: 1 } }],
-        },
-      },
-      { $unwind: "$user" },
-      { $set: { username: "$user.username" } },
-      { $unset: ["user"] },
-    ]);
-
-    return NextResponse.json({ success: true, leaderboardsData: top100 });
+    return NextResponse.json({ success: true, leaderboardsData: data });
   } catch (e) {
     return NextResponse.json({
       success: false,
---
+++ src/app/api/update-leaderboards/route.js
@@ -0,0 +1,74 @@
+import { Test } from "@/model/test";
+import dbConnect from "@/utils/dbConn";
+import { NextResponse } from "next/server";
+
+const modes = [
+  { text: "Words 10", mode: { name: "words", category: 10 } },
+  { text: "Words 25", mode: { name: "words", category: 25 } },
+  { text: "Words 50", mode: { name: "words", category: 50 } },
+  { text: "Words 100", mode: { name: "words", category: 100 } },
+  { text: "Time 15", mode: { name: "time", category: 15 } },
+  { text: "Time 30", mode: { name: "time", category: 30 } },
+  { text: "Time 60", mode: { name: "time", category: 60 } },
+  { text: "Time 120", mode: { name: "time", category: 120 } },
+];
+
+const getTop100 = async (mode) => {
+  return await Test.aggregate([
+    {
+      $match: {
+        "mode.name": mode.name,
+        "mode.category": mode.category,
+      },
+    },
+    {
+      $project: {
+        timeTaken: 0,
+        isPersonalBest: 0,
+        mode: 0,
+        language: 0,
+        __v: 0,
+        _id: 0,
+      },
+    },
+    { $sort: { wpm: -1 } },
+    { $group: { _id: "$userId", fastestTest: { $first: "$$ROOT" } } },
+    { $replaceRoot: { newRoot: "$fastestTest" } },
+    { $sort: { wpm: -1 } },
+    { $limit: 100 },
+    {
+      $lookup: {
+        from: "users",
+        localField: "userId",
+        foreignField: "_id",
+        as: "user",
+        pipeline: [{ $project: { _id: 0, username: 1 } }],
+      },
+    },
+    { $unwind: "$user" },
+    { $set: { username: "$user.username" } },
+    { $unset: ["user"] },
+  ]);
+};
+
+export async function GET() {
+  try {
+    const { connection: db } = await dbConnect();
+
+    for (let mode of modes) {
+      const updateDb = async () => {
+        const collection = db.collection(mode.text);
+        const top100 = await getTop100(mode.mode);
+        await collection.deleteMany({});
+        await collection.insertMany(top100);
+      };
+
+      updateDb();
+    }
+
+    return NextResponse.json({ message: "success" });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ error: "Something went wrong" });
+  }
+}
